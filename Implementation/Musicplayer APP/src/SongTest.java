b74c351fb6ff821feb85d19fb011ad24407e94f9
omkar jadhav <omkarjadhav1191@gmail.com>
2022-03-11 18:57:55 +0530

    new teat

--- Implementation/Musicplayer APP/src/SongTest.java
+++ Implementation/Musicplayer APP/src/SongTest.java
@@ -1,17 +1,26 @@
 package com.omkar;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class SongTest {
+class SongTest {
 	Song ceo = new Song("rio",3.4);
-	 
+	
 
 	@Test
-	public void testGetTitle() {
+	void testGetTitle() {
 		String expected = "rio";
 		String actual = ceo.getTitle();
+		assertEquals(expected,actual);
+		
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDuration() {
+		double expected = 3.4;
+		double actual = ceo.getDuration();
 		assertEquals(expected,actual);
 		//fail("Not yet implemented");
 	}
